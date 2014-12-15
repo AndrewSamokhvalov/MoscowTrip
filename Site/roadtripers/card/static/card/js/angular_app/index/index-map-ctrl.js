66517cfe09e0bc15e0b4caba68b9b4b73f85aca6
Ilya <gitrus@ya.ru>
2014-12-16 01:39:38 +0300

    1.Add new controller for filters. 2. New post queries in angular

--- Site/roadtripers/card/static/card/js/angular_app/index/index-map-ctrl.js
+++ Site/roadtripers/card/static/card/js/angular_app/index/index-map-ctrl.js
@@ -1,5 +1,41 @@
 angular.module('index-map-module')
-.controller('YMapController',[ '$scope', function($scope)
+.controller('YMapController',[ '$scope', '$rootScope', '$http', function($scope, $rootScope, $http)
 {
+    $scope.map = {
+        center: [55.75, 37.61], zoom: 10
+    };
 
-}]);
+    $scope.markers = [];
+    $rootScope.$watch('filter.length', function(oldValue,newValue)
+    {
+        if(oldValue != newValue)
+        {
+            $http.post('/places', {"filters": $rootScope.filter}).
+            success(function(data, status, headers, config)
+            {
+                console.log(data[1].coords);
+                $scope.markers = data.coords;
+            }).
+            error(function(data, status, headers, config)
+            {
+            });
+        }
+    });
+}])
+.controller('FilterController',['$scope' , '$rootScope', function ($scope , $rootScope)
+ {
+     $rootScope.filter =[];
+     $scope.functionWithBlackJackAndHoe = function (e)
+     {
+         console.log($rootScope.filter);
+         for (var i=0; i<$rootScope.filter.length; ++i)
+         {
+             if($rootScope.filter[i] == e)
+             {
+                 $rootScope.filter.splice(i,1);
+                 return false;
+             }
+         }
+        $rootScope.filter.push(e);
+     };
+ }]);
--- Site/roadtripers/card/static/card/js/angular_app/index/index-map-module.js
+++ Site/roadtripers/card/static/card/js/angular_app/index/index-map-module.js
@@ -1,1 +1,4 @@
-angular.module('index-map-module',['ymaps']);
+angular.module('index-map-module',['ymaps']).config(function(ymapsConfig) {
+    //нужно сменить preset у карты на специальный текстовый
+    ymapsConfig.markerOptions.preset = 'islands#darkgreenStretchyIcon';
+})
