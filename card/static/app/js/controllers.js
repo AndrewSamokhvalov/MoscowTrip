449723bba06db44bb48d4d82b7b42600275840b2
Ilya <gitrus@ya.ru>
2015-04-06 08:40:42 +0300

    rt Добавлена работа с фильтрами.

--- card/static/app/js/controllers.js
+++ card/static/app/js/controllers.js
@@ -1,83 +1,25 @@
 'use strict';
 
-
 roadtrippersApp.controller('CardCtrl', ['$scope', 'CardSvc',
     function ($scope, CardSvc)
     {
-        $scope.places = function(){ return $scope.rom.objects.getAll() };
+        $scope.filters = new MapObjectFilter($scope,CardSvc);
 
-        function Filters()
+        $scope.rom = new ROM($scope);
+
+        $scope.openModal = function()
         {
-            var filterArray = [];
-
-            function switchFilter(filter)
-            {
-                var indx = filter;
-                if (indx)
-                {
-                    var i = filterArray.indexOf(indx);
-                    console.log(i);
-                    if (i >= 0)
-                        filterArray.splice(i, 1);
-                    else
-                        filterArray.push(indx);
-                    CardSvc.setTypes($scope, filterArray);
-                }
-            }
-
-            return{
-                filters: filterArray,
-                switchFilter: function(i){switchFilter(i)}
-            }
+            document.getElementById('parent_popup').style.display='block';
+            document.getElementById('popup').style.display='block';
         };
 
-        $scope.places = function(){ return $scope.rom.objects.getAll() };
-
-        $scope.filters = new Filters();
-
-        $scope.createROM = function(){
-            this.rom = new ymaps.RemoteObjectManager('getPlaces?bbox=%b',
-                {
-                    clusterHasBalloon: false
-                });
-
-            this.rom.objects.events.add('add', function(child) {
-                $scope.$apply();
-            });
-
-            this.rom.objects.events.add('remove', function(child) {
-                $scope.$apply();
-            });
-
-            this.rom.events.add('click', function(child) {
-                console.log(child);
-            });
-
-            $scope.map.geoObjects.add(this.rom);
-
-        };
-
-        $scope.deleteROM = function(){
-            if(this.rom != null) {
-                $scope.map.geoObjects.remove(this.rom);
-//                $scope.$apply()
-            }
-
-        };
-
-        $scope.updateROM = function(){
-            $scope.deleteROM();
-            $scope.createROM();
-        };
-
-        $scope.init = function(map) {
+        $scope.init = function(map)
+        {
             $scope.map = map;
 
             ymaps.route(['москва метро пролетарская', 'Савёловский']).then(function (route) {
                 CardSvc.setRoute($scope, route);
             });
-
-            $scope.createROM();
 
             var zoomControl = new ymaps.control.ZoomControl({options: { position: { left: 5, top: 140 }}});
             var geolocationControl = new ymaps.control.GeolocationControl({options: { position: { left: 5, top: 105 }}})
@@ -95,10 +37,103 @@ roadtrippersApp.controller('CardCtrl', ['$scope', 'CardSvc',
             map.controls.add(routeEditor);
         };
 
-        $scope.placeUpdate = function(){
+        $scope.placeUpdate = function()
+        {
             $scope.places = [];
-            $scope.rom.objects.each(function(place){ $scope.places.push(place.fields)})
+            $scope.rom.rom.objects.each(function(place){ $scope.places.push(place.fields)})
         }
 
     }
 ]);
+
+function MapObjectFilter($scope,CardSvc)
+{
+    var filterArray = [];
+
+    function getFilterFromServer()
+    {
+        $.get('/getFilters')
+         .success( function (data)
+         {
+             filterArray = data;
+         });
+
+    }
+
+    function isChecked(indx)
+    {
+        var i = filterArray.indexOf(indx);
+        if (i >= 0)
+            return true;
+        return false;
+    }
+
+    function switchFilter(filter)
+    {
+        var indx = filter;
+        if (indx)
+        {
+            var i = filterArray.indexOf(indx);
+            if (i >= 0)
+                filterArray.splice(i, 1);
+            else
+                filterArray.push(indx);
+            CardSvc.setTypes($scope, filterArray);
+        }
+    }
+
+    return{
+        filters: filterArray,
+        switchFilter: function(i){switchFilter(i)},
+        isChecked: function(i){isChecked(i)}
+    }
+}
+
+function ROM($scope)
+{
+    var rom;
+
+    this.createROM = function()
+    {
+        rom = new ymaps.RemoteObjectManager('getPlaces?bbox=%b',
+            {
+                clusterHasBalloon: false
+            });
+
+        rom.objects.events.add('add', function(child) {
+            $scope.$apply();
+        });
+
+        rom.objects.events.add('remove', function(child) {
+            $scope.$apply();
+        });
+
+        rom.events.add('click', function(child) {
+            console.log(child);
+
+        });
+
+        $scope.map.geoObjects.add(rom);
+
+    };
+
+    var deleteROM = function deleteROM()
+    {
+        if(rom != null) {
+            $scope.map.geoObjects.remove(rom);
+//                $scope.$apply()
+        }
+
+    };
+
+    this.updateROM = function(){
+        deleteROM();
+        this.createROM();
+    };
+
+    this.rom = function()
+    {
+        if (!rom) this.createROM();
+        return rom
+    };
+}
